3b33b998fcf3f5e99b8cdfc462781c46c9ec474e
f4h2 <truonglocnguyenkhanh@gmail.com>
2025-07-25 17:25:47 +0700

    cai tien k8s

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 FROM python:3.10-slim
 
 WORKDIR /app
-COPY requirements.txt /app
-RUN pip install -r requirements.txt
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install gunicorn
+
 COPY . .
 
-CMD ["python", "app.py"]
+# Chạy với Gunicorn thay vì Flask dev server
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Kubernetes!"
+    app_name = os.getenv('APP_NAME', 'Default App')
+    api_key = os.getenv('API_KEY', 'No API Key')
+    return f"Hello from Kubernetes! App: {app_name}, API Key: {api_key}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
